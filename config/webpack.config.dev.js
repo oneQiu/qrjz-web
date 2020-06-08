96828df16cf1d1564e3776294cccfbe84d4f9123
One-Lemon <qq982516861@gmail.com>
2020-06-08 22:24:56 +0800

    Update 添加需要插件

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -3,20 +3,25 @@ const merge = require("webpack-merge");
 
 // webpack配置
 module.exports = merge(baseConfig, {
-  module: {
-    rules: [],
-  },
-  plugins: [],
-  resolve: {
-    alias: {},
-  },
-  devtool: "eval-source-map",
-  // 开发模式下的服务配置
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-    inline: true,
-    quiet:  true,
-    port: 8888
-  }
+	module: {
+		rules: [],
+	},
+	plugins: [],
+	resolve: {
+		alias: {},
+	},
+	devtool: "eval-source-map",
+	// 开发模式下的服务配置
+	devServer: {
+		contentBase: "./dist",
+		hot: true,
+		inline: true,
+		quiet: true,
+		port: 8888,
+		proxy: {
+			"/api": {
+				target: "http:127.0.0.1:1803",
+			},
+		},
+	},
 });
