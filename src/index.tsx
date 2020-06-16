5ecec83c3be7d68bbe689febdb2f25be664813fe
v_vflqiu <v_vflqiu@tencent.com>
2020-06-16 18:13:27 +0800

    Update 框架

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import "./styles/reset.less";
+import "antd/dist/antd.css";
+import "./styles/index.less";
+import "normalize.css"
 
 ReactDOM.render(
 	<Provider store={store}>
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import { Layout } from "antd";
+import "./index.less";
 
-export default class Index extends Component {
-	render() {
-		return (
-			<div>
-				123
-			</div>
-		);
-	}
+const { Header, Footer, Content, Sider } = Layout;
+
+interface Iprops {}
+interface Istate {
+  /**
+   * 是否可收起
+   */
+  collapsible: boolean;
+}
+export default class Index extends Component<Iprops, Istate> {
+  constructor(props: Iprops) {
+    super(props);
+    this.state = {
+      collapsible: true,
+    };
+  }
+  render() {
+    const { collapsible } = this.state;
+    return (
+      <Layout className="layout">
+        <Sider className="layout-sider" theme="light" collapsible={collapsible}>
+          4
+        </Sider>
+        <Layout>
+          <Header>1</Header>
+          <Footer>2</Footer>
+          <Content>3</Content>
+        </Layout>
+      </Layout>
+    );
+  }
 }
