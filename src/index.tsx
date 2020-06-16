db0c14ff4cc5eb7cefdd44d7b90b2af9d0d342c0
One-Lemon <qq982516861@gmail.com>
2020-06-16 23:28:34 +0800

    Update 头部

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import "./styles/reset.less";
+import "normalize.css";
 import "antd/dist/antd.css";
 import "./styles/index.less";
-import "normalize.css";
 
 ReactDOM.render(
 	<Provider store={store}>
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
-import { Layout } from "antd";
+import { Layout, Input } from "antd";
+import { render } from "react-dom";
+import { AudioOutlined } from "@ant-design/icons";
 import "./index.less";
 
 const { Header, Footer, Content, Sider } = Layout;
+const { Search } = Input;
+const suffix = (
+	<AudioOutlined
+		style={{
+			fontSize: 16,
+			color: "#1890ff",
+		}}
+	/>
+);
 
 interface Iprops {}
 interface Istate {
@@ -26,11 +37,44 @@ export default class Index extends Component<Iprops, Istate> {
 					4
 				</Sider>
 				<Layout>
-					<Header>1</Header>
+					<Header className="layout-header">
+						<HeaderInner />
+					</Header>
 					<Footer>2</Footer>
 					<Content>3</Content>
 				</Layout>
 			</Layout>
+		);
+	}
+}
+
+interface HeaderProps {}
+interface HeaderState {
+	/**
+	 * 搜索loading
+	 */
+	loading: boolean;
+}
+// 头部
+class HeaderInner extends Component<HeaderProps, HeaderState> {
+	constructor(props: HeaderProps) {
+		super(props);
+		this.state = {
+			loading: false,
+		};
+	}
+	render() {
+		const { loading } = this.state;
+		return (
+			<div>
+				<Search
+					loading={loading}
+					suffix={suffix}
+					placeholder="Search"
+					allowClear={true}
+					onSearch={(value) => console.log(value)}
+				/>
+			</div>
 		);
 	}
 }
